acdf587c1dcc18d6fb2ae6916121a5432a0e065e
Kótai Dániel <dkotai66@gmail.com>
2022-10-15 10:10:55 +0200

    szoveg korbefuttatasa megtortent

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import './style.css';
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap/dist/css/bootstrap.css";
 import './style.css';
+
+
+let urlText = document.getElementById('ultText');
+let fontSize = document.getElementById('fontSize');
+let lineHeight = document.getElementById('lineHeight');
