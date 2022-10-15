c4d39ca5acb9443aee1e33dc05d4c7dcace9c224
Kótai Dániel <dkotai66@gmail.com>
2022-10-15 12:17:20 +0200

    Feladat befejezve, mukodokepes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ import "bootstrap/dist/css/bootstrap.css";
 import './style.css';
 
 
-let urlText = document.getElementById('ultText');
+let urlText = document.getElementById('urlText');
 let fontSize = document.getElementById('fontSize');
 let lineHeight = document.getElementById('lineHeight');
+let fontColor = document.getElementById('fontColor');
+let button = document.getElementById('button');
+let pictureIMG = document.getElementById('picture');
+let text = document.getElementById('text');
+let div = document.getElementById('div');
+
+urlText.addEventListener('keyup', () =>{
+    pictureIMG.src = urlText.value;
+})
+
+fontSize.addEventListener('change', () =>{
+    text.style.fontSize = fontSize.value + "px";
+})
+
+lineHeight.addEventListener('change', () =>{
+    text.style.lineHeight = lineHeight.value + "em";
+})
+
+fontColor.addEventListener('change', () => {
+    text.style.color = fontColor.value;
+})
+
+button.addEventListener('click', () => {
+    div.classList.toggle('picture-active');
+});
+
+
